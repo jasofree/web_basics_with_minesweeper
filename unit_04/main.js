0ddb4ff6349378e1abcf074c578eb45c1b19c071
José Antonio Sánchez Ortiz <jasofree@gmail.com>
2022-02-02 19:43:52 +0100

    Avoid more than N bombs are created

--- unit_04/main.js
+++ unit_04/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let cellRows = 8;
     let cellColumns = 8;
+    let numberMinesRendered = 0;
     let numberMinesToDiscover = 10;
     let ratioMinesCells = Math.round(cellRows * cellColumns / numberMinesToDiscover);
     let gameCellsContainerNode = document.querySelector('#game_cells_container');
@@ -8,13 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let column = 0; column < cellColumns; column++) {
             let cellNode = document.createElement("div");
             let randomBomb = Math.random() * (ratioMinesCells + 1 - 1) + 1;
-            let isBomb = randomBomb <= 2;
+            let areThereMinesLeft = numberMinesRendered < numberMinesToDiscover;
+            let isBomb = areThereMinesLeft && randomBomb <= 2;
             cellNode.classList.add('game_cell');
             cellNode.setAttribute('data-row', row);
             cellNode.setAttribute('data-column', column);
             cellNode.setAttribute('data-is-bomb', isBomb);
             if (isBomb) {
                 cellNode.textContent = 'B';
+                numberMinesRendered++;
             }
             gameCellsContainerNode.appendChild(cellNode);
         }
