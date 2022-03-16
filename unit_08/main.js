bffdee16ef9a443e61f3fdbea96f45c6c196cf0c
José Antonio Sánchez Ortiz <jasofree@gmail.com>
2022-03-16 10:44:06 +0100

    Refactor code: function for retrieving cell node

--- unit_08/main.js
+++ unit_08/main.js
@@ -14,6 +14,10 @@ const gameProgressNode = document.querySelector('#game_progress');
 const gameProgressStateNode = gameProgressNode.querySelector('.state');
 gameProgressStateNode.innerHTML = thinkingCharacter;
 
+function getCellNode(row, column) {
+    return gameCellsContainerNode.querySelector(`.game_cell[data-row='${row}'][data-column='${column}']`);
+};
+
 function startGame() {
     for (let row = 0; row < cellRows; row++) {
         for (let column = 0; column < cellColumns; column++) {
@@ -34,7 +38,7 @@ function startGame() {
     }
     for (let row = 0; row < cellRows; row++) {
         for (let column = 0; column < cellColumns; column++) {
-            let cellNode = gameCellsContainerNode.querySelector(`.game_cell[data-row='${row}'][data-column='${column}']`);
+            let cellNode = getCellNode(row, column);
             if (cellNode.getAttribute('data-is-bomb') !== 'true') {
                 let numberNeighborBombs = countNeighborBombs(row, column);
                 cellNode.setAttribute('data-number-neighbor-bombs', numberNeighborBombs);
@@ -93,7 +97,7 @@ function countNeighborBombs(row, column) {
             }
             let neighbor_column = column + index_column;
             if (neighbor_row > -1 && neighbor_row < cellRows && neighbor_column > -1 && neighbor_column < cellColumns) {
-                let neighborNode = gameCellsContainerNode.querySelector(`.game_cell[data-row='${neighbor_row}'][data-column='${neighbor_column}']`);
+                let neighborNode = getCellNode(neighbor_row, neighbor_column);
                 let isBomb = neighborNode.getAttribute('data-is-bomb') === 'true';
                 if (isBomb) {
                     numberNeighborBombs++;
@@ -115,7 +119,7 @@ function clickNeighborsNoBombs(row, column) {
             }
             let neighbor_column = column + index_column;
             if (neighbor_row > -1 && neighbor_row < cellRows && neighbor_column > -1 && neighbor_column < cellColumns) {
-                let neighborNode = gameCellsContainerNode.querySelector(`.game_cell[data-row='${neighbor_row}'][data-column='${neighbor_column}']`);
+                let neighborNode = getCellNode(neighbor_row, neighbor_column);
                 let isBomb = neighborNode.getAttribute('data-is-bomb') === 'true';
                 let isCleared = neighborNode.classList.contains('cleared');
                 let numberNeighborBombs = parseInt(neighborNode.getAttribute('data-number-neighbor-bombs'));
